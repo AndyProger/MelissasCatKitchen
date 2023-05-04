22c468c4b28d04d01072c6409cc5ccf33a4ce41d
a.syradoiev <andrii.syradoiev@nure.ua>
2023-05-04 22:59:34 +0300

    more animations

--- Assets/Scripts/PlayerScripts/Animations/PlayerAnimator.cs
+++ Assets/Scripts/PlayerScripts/Animations/PlayerAnimator.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     private static readonly int Running = Animator.StringToHash("Running");
+    private static readonly int HoldAndRun = Animator.StringToHash("HoldAndRun");
     private static readonly int Idle = Animator.StringToHash("Idle");
+    private static readonly int IdleAndHold = Animator.StringToHash("IdleAndHold");
 
     [SerializeField] private Animator _animator;
 
@@ -15,6 +17,13 @@ public class PlayerAnimator : MonoBehaviour
 
     private void SetWalkingMode(bool isWalking)
     {
+        if (Player.Instance.HasKitchenObject())
+        {
+            _animator.SetTrigger(isWalking ? HoldAndRun : IdleAndHold);
+            _animator.ResetTrigger(!isWalking ? HoldAndRun : IdleAndHold);
+            return;
+        }
+        
         _animator.SetTrigger(isWalking ? Running : Idle);
         _animator.ResetTrigger(!isWalking ? Running : Idle);
     }
